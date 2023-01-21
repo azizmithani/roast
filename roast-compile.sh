1902a3adc4a35f5a637b47fe7bdd9ce49d63d0df
Madiha Shabbir <73662155+Shabbirm21@users.noreply.github.com>
2023-01-21 17:15:21 +0000

    Add files via upload
    Bash script to compile roast

---
+++ roast-compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+bamtools=~/miniconda2/envs/roast-env2
+boost=~/miniconda2/envs/roast-env2
+
+g++ -I $bamtools/include/bamtools -I $boost/include/ -L $boost/lib/ -L $bamtools/lib/ ROAST_extendContigs.cpp -o ROAST_extendContigs -lbamtools -lboost_filesystem -lboost_regex -lz
+
+g++ -I $bamtools/include/bamtools -I $boost/include/ -L $boost/lib/ -L $bamtools/lib/ ROAST_extendContigs_SCs.cpp -o ROAST_extendContigs_SCs -lbamtools -lboost_filesystem -lboost_regex -lz
+
+g++ -I $bamtools/include/bamtools -I $boost/include/ -L $boost/lib/ -L $bamtools/lib/ ROAST_mergeContigs_SCs.cpp -o ROAST_mergeContigs_SCs -	lbamtools -lboost_filesystem -lboost_regex -lz
+
+g++ -o roast -I $bamtools/include/bamtools -I $boost/include/ -L $boost/lib/ -L $bamtools/lib/ main.cpp mis_assembly_chimera.cpp global.cpp filterSamToFastq.cpp bySoftclip.cpp byRI.cpp alignment.cpp -lbamtools -lboost_filesystem -lboost_regex -lz
