8ddfa2a3b9b38b22761151e4ec4c94070f2d6667
Madiha Shabbir <73662155+Shabbirm21@users.noreply.github.com>
2023-02-05 21:08:25 +0000

    Update roast-compile.sh

--- roast-compile.sh
+++ roast-compile.sh
@@ -2,6 +2,8 @@
 bamtools=~/miniconda2/envs/roast-env2
 boost=~/miniconda2/envs/roast-env2
 
+unzip external_tools/cufflinks
+
 g++ -I $bamtools/include/bamtools -I $boost/include/ -L $boost/lib/ -L $bamtools/lib/ ROAST_extendContigs.cpp -o ROAST_extendContigs -lbamtools -lboost_filesystem -lboost_regex -lz
 
 g++ -I $bamtools/include/bamtools -I $boost/include/ -L $boost/lib/ -L $bamtools/lib/ ROAST_extendContigs_SCs.cpp -o ROAST_extendContigs_SCs -lbamtools -lboost_filesystem -lboost_regex -lz
